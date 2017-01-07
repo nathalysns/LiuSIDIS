ffebac7164131498a8616a9338d4ee9cf2f74bfc
TianboLiu <liutianbo1987@gmail.com>
2017-01-07 02:06:28 -0500

    Update analysis_proton.C

--- SoLID/analysis_proton.C
+++ SoLID/analysis_proton.C
@@ -1,11 +1,11 @@
-#include "SoLID_SIDIS_3He.h"
+#include "SoLID_SIDIS_NH3.h"
 
 using namespace std;
 
 int main(int argc, char * argv[]){
 
   if (argc < 2){
-    cout << "./analysis_neutron <opt>" << endl;
+    cout << "./analysis_proton <opt>" << endl;
     cout << "opt = 0: total rate" << endl;
     cout << "     ./analysis 0" << endl;
     cout << "     ./analysis 0 <R0>" << endl;
@@ -34,10 +34,11 @@ int main(int argc, char * argv[]){
     GetTotalRate(8.8, "pi-");
   }
 
+	/*
   if (opt == 1){
     if (argc == 2){
-      GenerateBinInfoFile("NeutronResults/bin_info_N11p.dat", 11.0, "pi+");
-      GenerateBinInfoFile("NeutronResults/bin_info_N8p.dat", 8.8, "pi+");
+      GenerateBinInfoFile("ProtonResults/bin_info_N11p.dat", 11.0, "pi+");
+      GenerateBinInfoFile("ProtonResults/bin_info_N8p.dat", 8.8, "pi+");
       GenerateBinInfoFile("NeutronResults/bin_info_N11m.dat", 11.0, "pi-");
       GenerateBinInfoFile("NeutronResults/bin_info_N8m.dat", 8.8, "pi-");
     }
@@ -80,7 +81,7 @@ int main(int argc, char * argv[]){
     double MfT2 = atof(argv[4]);
     CheckCurrentCut(11.0, "pi+", kT2, MiT2, MfT2);
   }
-
+*/
 
   return 0;
 }
