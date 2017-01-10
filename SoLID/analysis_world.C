a47056eaea347d1849f689e453195ee190f760de
TianboLiu <liutianbo1987@gmail.com>
2017-01-10 02:08:58 -0500

    Update analysis_world.C

--- SoLID/analysis_world.C
+++ SoLID/analysis_world.C
@@ -51,7 +51,25 @@ int main(int argc, char * argv[]){
     getline(infile, hadron, ',');
     getline(infile, experiment, '\n');
 	
-    if (target == "proton") mysidis.SetNucleus(1,0);
+    if (target == "proton") mysidis.SetNucleus(1, 0);
+    else if (target == "neutron") mysidis.SetNucleus(0, 1);
+    else if (target == "deuteron") mysidis.SetNucleus(1, 1);
+	
+    if (hadron == "pi+" || hadron == "h+") mysidis.SetHadron("pi+");
+    else if (hadron == "pi-" || hadron == "h-") mysidis.SetHadron("pi-");
+    else if (hadron == "pi0") mysidis.SetHadron("pi0");
+    else if (hadron == "k+") mysidis.SetHadron("K+");
+    else if (hadron == "k-") mysidis.SetHadron("K-");
+    else if (hadron == "k0") mysidis.SetHadron("K0");
+	
+    l.SetXYZT(0, 0, Ebeam, Ebeam);
+    P.SetXYZT(0, 0, 0, 0.938272);
+    mysidis.SetInitialState(l, P);
+    
+    mysidis.SetVariables(x, y, z, Q2, pT, 0.0, 0.0);
+    mysidis.CalculateFinalState();
+
+    cout << mysidis.GetVariable("W") << endl;
   //}
   printf("%.0f,%.1f,%.6f,%.6f,%.6f,%.6f,%.6f,%s,%.1f,%.6f,%.6f,%.6f,%s,%s,%s\n",
 	      i, Ebeam, x, y, z, Q2, pT, obs.data(), value, stat, systrel, systabs, target.data(), hadron.data(), experiment.data());
