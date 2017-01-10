d3bba4ec0754ff7cb5acca992b24b3bea34579e7
TianboLiu <liutianbo1987@gmail.com>
2017-01-10 01:57:57 -0500

    Update analysis_world.C

--- SoLID/analysis_world.C
+++ SoLID/analysis_world.C
@@ -16,6 +16,7 @@ int main(int argc, char * argv[]){
   
   double i, Ebeam, x, y, z, Q2, pT, value, stat, systrel, systabs;
   string obs, target, hadron, experiment;
+  double R;
  
   FILE * file = fopen(argv[2], "w");
   
@@ -23,10 +24,35 @@ int main(int argc, char * argv[]){
   TLorentzVector l, P;
   int j = 0;
   //while (infile.good()){
-    getline(infile, i, ',');
+    getline(infile, tmp, ',');
+    i = atof(tmp);
+    getline(infile, tmp, ',');
+    Ebeam = atof(tmp);
+    getline(infile, tmp, ',');
+    x = atof(tmp);
+    getline(infile, tmp, ',');
+    y = atof(tmp);
+    getline(infile, tmp, ',');
+    z = atof(tmp);
+    getline(infile, tmp, ',');
+    Q2 = atof(tmp);
+    getline(infile, tmp, ',');
+    pT = atof(tmp);
+    getline(infile, obs, ',');
+    getline(infile, tmp, ',');
+    value = atof(tmp);
+    getline(infile, tmp, ',');
+    stat = atof(tmp);
+    getline(infile, tmp, ',');
+    systrel = atof(tmp);
+    getline(infile, tmp, ',');
+    systabs = atof(tmp);
+    getline(infile, target, ',');
+    getline(infile, hadron, ',');
+    getline(infile, experiment, '\n');
   //}
-  
-  printf("%s\n", tmp.data());
+  printf("%.0f,%.1f,%.6f,%.6f,%.6f,%.6f,%.6f,%s,%.1f,%.6f,%.6f,%.6f,%s,%s,%s\n",
+	      i, Ebeam, x, y, z, Q2, Pt, obs.data(), value, stat, systrel, systabs, target.data(), hadron.data(), experiment.data());
   
   infile.close();
 
