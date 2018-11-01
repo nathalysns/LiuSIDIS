7d44905b71a49109c212a7157ca9ec102b7a3eb6
User <user@UsermatoMacBook-Pro-2.local>
2018-11-01 16:51:51 -0400

    update header

--- Header/Lsidis2.h
+++ Header/Lsidis2.h
@@ -3,7 +3,7 @@
 //LHAPDF is required. Developed with v6.1
 //ROOT is required. Developed with v5.34.21
 //Potential error with other versions not tested
-//Last update date 6 Dec. 2017 version 2.2
+//Last update date 1 Nov. 2018 version 2.3
 
 #ifndef _LSIDIS_H_
 #define _LSIDIS_H_
@@ -21,26 +21,28 @@
 #include "TMatrixD.h"
 #include "TH1D.h"
 
+const double PI = 3.14159265358979323846264;//math Pi
+const double GeVfm = 1.0 / 0.1973269718;//GeV times fm in natural unit
+const double GeV2nb = 1.0 / 389379.338;//GeV^2 times nb in natural unit
+const double alpha_em = 1.0 / 137.035999074;//EM fine-structure constant
+const double eu2 = 4.0 / 9.0;//up type quark charge square
+const double ed2 = 1.0 / 9.0;//down type quark charge square
+const double Me = 0.000510998928;//electron mass in GeV
+const double Mmu = 0.1056583715;//muon mass in GeV
+const double Mp = 0.938272081;//proton mass in GeV
+const double Mn = 0.939565413;//neutron mass in GeV
+const double MLambda = 1.115683;//Lambda mass in GeV
+const double MSigmap = 1.18937;//Sigma+ mass in GeV
+const double MSigmam = 1.197449;//Sigma- mass in GeV
+const double Mpion = 0.13957018;//charged pion mass in GeV
+const double Mpi0 = 0.1349766;//neutral pion mass in GeV
+const double Mkaon = 0.493677;//charged kaon mass in GeV
+const double MK0 = 0.497614;//neutral kaon mass in GeV
+const double Meta = 0.77526;//rho mass in GeV
+
 class Lsidis{
  protected:
-  static const double PI = 3.14159265358979323846264;//math Pi
-  static const double GeVfm = 1.0 / 0.1973269718;//GeV times fm in natural unit
-  static const double GeV2nb = 1.0 / 389379.338;//GeV^2 times nb in natural unit
-  static const double alpha_em = 1.0 / 137.035999074;//EM fine-structure constant
-  static const double eu2 = 4.0 / 9.0;//up type quark charge square
-  static const double ed2 = 1.0 / 9.0;//down type quark charge square
-  static const double Me = 0.000510998928;//electron mass in GeV
-  static const double Mmu = 0.1056583715;//muon mass in GeV
-  static const double Mp = 0.938272081;//proton mass in GeV
-  static const double Mn = 0.939565413;//neutron mass in GeV
-  static const double MLambda = 1.115683;//Lambda mass in GeV
-  static const double MSigmap = 1.18937;//Sigma+ mass in GeV
-  static const double MSigmam = 1.197449;//Sigma- mass in GeV
-  static const double Mpion = 0.13957018;//charged pion mass in GeV
-  static const double Mpi0 = 0.1349766;//neutral pion mass in GeV
-  static const double Mkaon = 0.493677;//charged kaon mass in GeV
-  static const double MK0 = 0.497614;//neutral kaon mass in GeV
-  static const double Meta = 0.77526;//rho mass in GeV
+
  private:
   bool st_nucleus;
   bool st_pdf;
@@ -846,6 +848,10 @@ int Lsidis::Test(){//Test code
    
    update log from v2.0 to 2.1
    fix pid bug
+
+   update log from v2.2 to 2.3
+   move static const double variables outside class definition
+
 */
 
 
